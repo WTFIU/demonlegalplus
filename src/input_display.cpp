671582e7c8af9cca2c5c81580c88c01ecc2fde19
WTFIU <jackpants0913alt@gmail.com>
2025-07-28 15:40:33 -0400

    Update input_display.cpp

--- src/input_display.cpp
+++ src/input_display.cpp
@@ -1,1 +1,14 @@
+#include "input_display.h"
+#include <iostream>
+
+namespace input_display {
+    void init() {
+        std::cout << "[DemonLegal+] Initialized input display.\n";
+        // TODO: ImGui init here
+    }
+
+    void render() {
+        // TODO: ImGui render calls here
+    }
+}
 
